78423a830f7093919a686e22bd9fa56061037be3
Joseph Cottingham <joseph.cottingham@protonmail.com>
2022-04-26 17:25:04 -0400

    comments

--- run.go
+++ run.go
@@ -11,14 +11,13 @@ import (
 	"encoding/json"
 	"math/rand"
 	"errors"
-	// "strings"
 	"io"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"net/http"
 )
 
-
+// Store device for each book key data and utlized in JSON export.
 type Book struct {
 	Title string `json:"title"`
 	Author string `json:"author"`
@@ -29,6 +28,7 @@ type Book struct {
 	Audio_download_url string `json:"Audio_download_url"`
 }
 
+// Downloads a resource for a given URL to given location on disk.
 func DownloadFile(filepath string, url string) error {
 
 	// Get the data
@@ -50,6 +50,7 @@ func DownloadFile(filepath string, url string) error {
 	return err
 }
 
+// Generates a random string of a given length
 func randomString(length int) string {
     rand.Seed(time.Now().UnixNano())
     b := make([]byte, length)
@@ -57,6 +58,7 @@ func randomString(length int) string {
     return fmt.Sprintf("%x", b)[:length]
 }
 
+// Save string as file in a given directory with a given file name.
 func save(htmlString string, fileName string, outputDir string) {
 	createDir(outputDir)
 	htmlDir := outputDir + "/" + fileName
@@ -66,6 +68,7 @@ func save(htmlString string, fileName string, outputDir string) {
 	htmlFile.WriteString(htmlString)
 }
 
+// Checks if a directory is exist and if it does not then create the directory.
 func createDir(outputDir string) {
 	if _, err := os.Stat(outputDir); errors.Is(err, os.ErrNotExist) {
 		err := os.Mkdir(outputDir, os.ModePerm)
@@ -84,7 +87,7 @@ func main() {
 	// tracks the current search page (pagniation on the website)
 	var cur_page_counter int16 = 0
 
-	// This max page counter is set as of 5/14/2022 and is subject to change (1516)
+	// This max page counter is set as of 5/14/2022 and is subject to change (1516) (english only)
 	var max_page_counter int16 = 1516
 
 	var book_pages []string
@@ -97,18 +100,23 @@ func main() {
 	// ctxt, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Collect book detail view url's on each of the search pages
 	for cur_page_counter <= max_page_counter {
+		// interate the page
 		cur_page_counter++
 		fmt.Printf("Page %d\n", cur_page_counter)
+		// create temp url for the page of intrest in this iteration
 		search_url := "https://librivox.org/search?primary_key=1&search_category=language&search_page=" + strconv.FormatInt(int64(cur_page_counter), 10) + "&search_form=get_results"
 
 		fmt.Printf("%s\n", search_url)
 		
+		// Scrape all book open buttons on this page.
 		err := chromedp.Run(ctxt, collectBookUrls(search_url, &nodes))
 		if err != nil {
 			fmt.Println(err)
 		}
 
+		// Collect each href for the scaped button
 		for _, n := range nodes {
 			book_pages = append(book_pages, n.AttributeValue("href"))
 		}
@@ -116,53 +124,47 @@ func main() {
 		fmt.Printf("Complete %d\n", cur_page_counter)
 	}
 
-	
+	// Create custom directory that ties output to time
 	createDir(outDir)
 	outDir=outDir+"/"+time.Now().Format("2006-01-02-15:04:05")
 	createDir(outDir)
 
+	// Collect book information for each detail page and save it to a file, and save the audio zip.
 	for _, book_url := range book_pages {
 		fmt.Println(book_url)
+		// Stores the book
 		b := Book{}
-		var nodesBtn []*cdp.Node
+		// Scape the site for data
 		err := chromedp.Run(ctxt, collectBookData(book_url, &b.Title, &b.Author, &b.Reader, &b.Language, &b.Genre, &b.Audio_file_count, &b.Audio_download_url))
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		fmt.Println(len(nodesBtn))
-		for _, n := range nodesBtn {
-			fmt.Println("Test")
-			fmt.Println(n.AttributeValue("href"))
-		}
-
+		// Format struct as json string
 		j, err := json.Marshal(&b)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(string(j))
 
+		// Save the meta data about the book
 		tempDir := outDir+"/"+randomString(16)
-
 		save(string(j), "meta.json", tempDir)
 			
-
-		fmt.Printf("Complete %s\n", b.Title)
+		// Download the zip file that holds the audio (leave compressed so that it is more mobile)
 		err = DownloadFile(tempDir+"audio.zip", b.Audio_download_url)
 		if err != nil {
 			panic(err)
 		}
-		// fmt.Printf("%s %s %s %s %s\n", author, reader, language, genre, audio_file_count)
+		fmt.Printf("Complete %s\n", b.Title)
 	}
 
-	// err = chromedp.Shutdown(ctxt)
+	err = chromedp.Shutdown(ctxt)
 
-	// err = chromedp.Wait()
+	err = chromedp.Wait()
 }
 
-
-
+// Collect the url of the detail view page of each book.
 func collectBookUrls(url string, nodes *[]*cdp.Node) chromedp.Tasks{
 	// Gather all the book urls on a single page
 	return chromedp.Tasks{
@@ -172,7 +174,7 @@ func collectBookUrls(url string, nodes *[]*cdp.Node) chromedp.Tasks{
 	}
 }
 
-
+// Gather data about book on book detail page.
 func collectBookData(url string, title *string, author *string, reader *string, language *string, genre *string, audio_file_count *string, audio_download_url *string) chromedp.Tasks{
 	// Gather all the book urls on a single page
 	var ok bool
@@ -189,7 +191,3 @@ func collectBookData(url string, title *string, author *string, reader *string, 
 		// chromedp.Text(".listen-download dd:nth-child(2) .book-download-btn", audio_download_url),
 	}
 }
-
-
-
-
